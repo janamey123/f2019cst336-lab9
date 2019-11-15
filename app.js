369653c4d11358cfa9e1b2c7fa1ea2172a9826a8
Jana Meyer <janmeyer@csumb.edu>
2019-11-14 22:51:53 -0800

    lab 9

--- app.js
+++ app.js
@@ -6,25 +6,54 @@ app.use(express.static("public")); //folder for images, css, js
 
 // routes
 app.get("/", function (req, res) {
-    res.send("it works!");
+    res.render("index");
 }); //root
 
+app.get("/quotes", async function (req, res) {
+    let keyword = req.query.keyword;
+    console.log(keyword);
+
+    let rows = await getQuotes();
+
+    res.render("quotes", {"records": rows});
+}); //root
+
+function getQuotes() {
+    let conn = dbConnection();
+
+    return new Promise(function (resolve, reject) {
+        conn.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+            let sql = ` SELECT quote, lastName, category
+                    FROM l9_quotes
+                    NATURAL JOIN l9_author
+                    WHERE quote LIKE '%${keyword}%'
+                    `;
+            conn.query(sql, function (err, rows, fields) {
+                if (err) throw err;
+                //res.send(rows);
+            });
+        });//connect
+    });//promise
+}//getQuotes
+
 app.get("/dbTest", function (req, res) {
-   let conn = dbConnection();
+    let conn = dbConnection();
 
-   conn.connect(function (err) {
-       if (err) throw err;
-       console.log("Connected!");
-       let sql = "SELECT * FROM l9_author WHERE sex = 'F'";
+    conn.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        let sql = "SELECT * FROM l9_author WHERE sex = 'F'";
 
-       conn.query(sql, function (err, rows, fields) {
-           if (err) throw err;
-           res.send(rows);
-       });
-   });
+        conn.query(sql, function (err, rows, fields) {
+            if (err) throw err;
+            res.send(rows);
+        });
+    });
 });//dbTest
 
-function dbConnection(){
+function dbConnection() {
     let conn = mysql.createConnection({
         host: "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         user: "vr51fr35ztycmd35",
@@ -33,7 +62,6 @@ function dbConnection(){
     });//createConnection
     return conn;
 }
-
 
 
 // starting server
